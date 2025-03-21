690ed7301b68530061f0bd21a49436145c15b3f3
hichamoubaha <oubahahicham7@gmail.com>
2025-03-21 14:35:43 +0000

    add statistics for recruteur

--- app/Http/Controllers/StatistiquesController.php
+++ app/Http/Controllers/StatistiquesController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Log;
 
 class StatistiquesController extends Controller
 {
-    // Statistiques pour le recruteur (annonces et candidatures)
+    // Statistiques pour le recruteur 
     public function statistiquesRecruteur()
     {
         try {
